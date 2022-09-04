6432ea899d029eee3c116966644675d5401f6753
Marko Hrncic <marko.hrncic@kogitas.com>
2022-09-04 19:50:46 +0200

    Implemented first versions of YouTube and IMDB API external services in the Infrastructure layer.

---
+++ Movies/Imdb.External/APIs/ImdbApiService.cs
@@ -0,0 +1,23 @@
+﻿using IMDbApiLib;
+using Microsoft.Extensions.Configuration;
+using Movies.Domain;
+
+namespace DataProvider.External.APIs
+{
+    public class ImdbApiService
+    {
+        private readonly ApiLib _imdbApi;
+
+        public ImdbApiService(IConfiguration configuration)
+        {
+            _imdbApi = new ApiLib(configuration["ImdbAPI:APIKey"]);
+        }
+
+        public async Task<ICollection<Movie>> GetMovies(string keyword)
+        {
+            var movies = await _imdbApi.SearchMovieAsync(keyword);
+
+            return null;
+        }
+    }
+}
---
+++ Movies/Imdb.External/APIs/YouTubeApiService.cs
@@ -0,0 +1,41 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Microsoft.Extensions.Configuration;
+using Movies.Domain;
+
+namespace DataProvider.External.APIs
+{
+    public class YouTubeApiService
+    {
+        private readonly YouTubeService _youTubeService;
+        private readonly IConfiguration _config;
+
+        public YouTubeApiService(IConfiguration configuration)
+        {
+            _config = configuration;
+
+            _youTubeService =  new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = _config["YouTubeAPI:APIKey"],
+                ApplicationName = _config["YouTubeAPI:AppName"]
+            });
+        }
+
+        public async Task<ICollection<Trailer>> GetTrailersForMovie(string movieName)
+        {
+            var searchRequest = GetYouTubeRequest(movieName);
+            var response = await searchRequest.ExecuteAsync();
+
+            return null;
+        }
+
+        private SearchResource.ListRequest GetYouTubeRequest(string movieName)
+        {
+            var searchRequest = _youTubeService.Search.List("snippet");
+            searchRequest.Q = $"Trailer {movieName}";
+            searchRequest.MaxResults = Int32.Parse(_config["YouTubeAPI:MaxResults"]);
+
+            return searchRequest;
+        }
+    }
+}
