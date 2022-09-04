22284a5cfa143895b27cd60c827380710e552519
Marko Hrncic <marko.hrncic@kogitas.com>
2022-09-04 17:07:39 +0200

    Implemented extension method for configuring services in the Application layer.

---
+++ Movies/Movies.Application/Config/ApplicationServicesConfig.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Movies.Application.Config
+{
+    public static class ApplicationServicesConfig
+    {
+        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
