69f3263eb432632f44fc44a5f55515656192d19d
Marko Hrncic <marko.hrncic@kogitas.com>
2022-09-04 17:13:49 +0200

    Implemented Controller to retrieve Movies data.

---
+++ Movies/Movies.API/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Application.DTOs;
+using Movies.Application.Features.Requests;
+
+namespace Movies.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MoviesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{keyword}")]
+        public async Task<ActionResult<MovieResultDto>> GetMovies(string keyword)
+        {
+            var leaveTypes = await _mediator.Send(new GetMoviesRequest(keyword));
+            return Ok(leaveTypes);
+        }
+    }
+}
--- Movies/Movies.Application/Features/Requests/GetMoviesRequest.cs
+++ Movies/Movies.Application/Features/Requests/GetMoviesRequest.cs
@@ -5,6 +5,11 @@ namespace Movies.Application.Features.Requests
 {
     public class GetMoviesRequest: IRequest<MovieResultDto>
     {
+        public GetMoviesRequest(string keyword)
+        {
+            Keyword = keyword;
+        }
+
         public string Keyword { get; set; } = string.Empty;
     }
 }
